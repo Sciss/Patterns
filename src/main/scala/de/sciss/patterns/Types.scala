7e1065a6524dcfb02265ac383fa0d4c7cf5acc32
Hanns Holger Rutz <contact@sciss.de>
2017-11-07 10:43:32 +0100

    nope

--- src/main/scala/de/sciss/patterns/Types.scala
+++ src/main/scala/de/sciss/patterns/Types.scala
@@ -1,34 +1,42 @@
 package de.sciss.patterns
 
 import scala.collection.AbstractIterator
+import scala.language.implicitConversions
 
 object Types {
-  trait Top {
-    type Out
+  trait Top[A] {
+//    type Out
   }
 
-  trait Elem[A <: Top] {
-    def mkIter: Iterator[A#Out]
+  trait Elem[A, +T <: Top[A]] {
+    val tpe: T
+
+    def mkIter: Iterator[A]
   }
 
-  sealed trait NumTop[Out1] extends Top {
-    final type Out = Out1
-    def num: Numeric[Out]
+  sealed trait NumTop[A] extends Top[A] {
+    def num: Numeric[A]
+
+//    def plus(x: A, y: A): A
   }
 
-  sealed trait IntLikeTop[Out1] extends NumTop[Out1]
+  sealed trait IntLikeTop[A] extends NumTop[A]
 
   sealed trait IntTop extends IntLikeTop[Int] {
     def num: Numeric[Out] = ???
+    final type Out = Int
+    def plus(x: Out, y: Out): Out = x + y
   }
   implicit object IntTop extends IntTop
 
   sealed trait IntSeqTop extends IntLikeTop[Seq[Int]] {
+    final type Out = Seq[Int]
     def num: Numeric[Out] = ???
+    def plus(x: Out, y: Out): Out = (x, y).zipped.map(_ + _)
   }
   implicit object IntSeqTop extends IntSeqTop
 
-  final case class Foo[A, T <: NumTop[A]](a: Elem[T], b: Elem[T])(implicit tpe: T) extends Elem[T] {
+  final case class Foo[A, T <: NumTop[A]](a: Elem[A, T], b: Elem[A, T])(implicit val tpe: T) extends Elem[A, T] {
     def mkIter: Iterator[A] = {
       val ai = a.mkIter
       val bi = b.mkIter
@@ -45,10 +53,10 @@ object Types {
     }
   }
 
-  implicit def intElem    (i: Int     ): Elem[IntTop]     = ???
-  implicit def intSeqElem (i: Seq[Int]): Elem[IntSeqTop]  = ???
+  implicit def intElem    (i: Int     ): Elem[Int, IntTop]     = ???
+  implicit def intSeqElem (i: Seq[Int]): Elem[Seq[Int], IntSeqTop]  = ???
 
   def example(): Unit = {
-    Foo(Seq(1, 2), 3)
+//    Foo[IntLikeTop[_]](Seq(1, 2), 3)
   }
 }
