9c03528f212a041ba3ca24947e9c6b23ee078930
Hanns Holger Rutz <contact@sciss.de>
2017-11-07 12:20:06 +0100

    ya, we don't need covariance then

--- src/main/scala/de/sciss/patterns/Types.scala
+++ src/main/scala/de/sciss/patterns/Types.scala
@@ -4,32 +4,28 @@ import scala.collection.AbstractIterator
 import scala.language.implicitConversions
 
 object Types {
+  def main(args: Array[String]): Unit = example()
+
   trait Top {
     type Out
   }
 
-  trait Bridge[In1, In2, Out] {
-    def plus(a: In1, b: In2): Out
-  }
-
-  trait Elem[+T <: Top] {
+  trait Num[T1 <: Top, T2 <: Top, T <: Top] {
     val tpe: T
 
-    def mkIter: Iterator[tpe.Out]
+    def lift1(a: T1#Out): tpe.Out
+    def lift2(a: T2#Out): tpe.Out
+
+    def plus(a: tpe.Out, b: tpe.Out): tpe.Out
   }
 
-  sealed trait NumTop extends Top {
-//    def num: Numeric[A]
+  trait Elem[T <: Top] {
+    val tpe: T
 
-//    def bridge[In1 <: A, In2 <: A](a: Top[In1], b: Top[In2]): Bridge[In1, In2, A]
-    def bridge(a: NumTop, b: NumTop): Bridge[a.Out, b.Out, Out]
-
-//    def plus(x: A, y: A): A
+    def iterator: Iterator[tpe.Out]
   }
 
-  sealed trait IntLikeTop extends NumTop {
-    def bridge(a: NumTop, b: NumTop): Bridge[a.Out, b.Out, Out] = ???
-  }
+  sealed trait IntLikeTop extends Top
 
   sealed trait IntSeqTop extends IntLikeTop {
     type Out = Seq[Int]
@@ -48,26 +44,95 @@ object Types {
 //
 //  sealed trait DoubleTop extends DoubleSeqTop
 
-  final case class Add[T <: NumTop](a: Elem[T], b: Elem[T])(implicit val tpe: T) extends Elem[T] {
-    def mkIter: Iterator[tpe.Out] = {
-      val ai = a.mkIter
-      val bi = b.mkIter
-      val br = tpe.bridge(a.tpe, b.tpe)
+  implicit object intNum extends Num[IntTop, IntTop, IntTop] {
+    val tpe: IntTop = IntTop
+
+    def lift1(a: Int): Int = a
+    def lift2(a: Int): Int = a
+
+    def plus(a: Int, b: Int): Int = a + b
+  }
+
+  trait IntLikeNum {
+    final val tpe: IntSeqTop = IntSeqTop
+
+    final def plus(a: Seq[Int], b: Seq[Int]): Seq[Int] = {
+      val as = a.size
+      val bs = b.size
+      val sz = math.max(as, bs)
+      Seq.tabulate(sz) { i =>
+        a(i % as) + b(i % bs)
+      }
+    }
+  }
+
+  implicit object intSeqNum1 extends IntLikeNum with Num[IntTop, IntSeqTop, IntSeqTop] {
+    def lift1(a: Int     ): Seq[Int] = a :: Nil
+    def lift2(a: Seq[Int]): Seq[Int] = a
+  }
+
+  implicit object intSeqNum2 extends IntLikeNum with Num[IntSeqTop, IntTop, IntSeqTop] {
+    def lift1(a: Seq[Int]): Seq[Int] = a
+    def lift2(a: Int     ): Seq[Int] = a :: Nil
+  }
+
+  implicit object intSeqNum3 extends IntLikeNum with Num[IntSeqTop, IntSeqTop, IntSeqTop] {
+    def lift1(a: Seq[Int]): Seq[Int] = a
+    def lift2(a: Seq[Int]): Seq[Int] = a
+  }
+
+  final case class Add[T1 <: Top, T2 <: Top, T <: Top](a: Elem[T1], b: Elem[T2])
+                                                      (implicit val br: Num[T1, T2, T])
+    extends Elem[T] {
+
+    val tpe: br.tpe.type = br.tpe
+
+    def iterator: Iterator[tpe.Out] = {
+      val ai = a.iterator
+      val bi = b.iterator
 
       new AbstractIterator[tpe.Out] {
         def hasNext: Boolean = ai.hasNext && bi.hasNext
 
         def next(): tpe.Out = {
-          val an = ai.next()
-          val bn = bi.next()
-          br.plus(an, bn)
+          val an  = br.lift1(ai.next())
+          val bn  = br.lift2(bi.next())
+          val res = br.plus(an, bn)
+          res
         }
       }
     }
   }
 
-  final case class Cat[T <: Top, T1 <: T, T2 <: T](a: Elem[T1], b: Elem[T2])(implicit val tpe: T) extends Elem[T] {
-    def mkIter: Iterator[tpe.Out] = ??? // a.mkIter ++ b.mkIter
+  final case class Series[T1 <: Top, T2 <: Top, T <: Top](list: Elem[T1], step: Elem[T2])
+                                                         (implicit val br: Num[T1, T2, T])
+    extends Elem[T] {
+
+    val tpe: br.tpe.type = br.tpe
+
+    def iterator: Iterator[tpe.Out] = {
+      val ai = list.iterator
+      val bi = step.iterator
+
+      if (ai.isEmpty) Iterator.empty
+      else new AbstractIterator[tpe.Out] {
+        private var state: tpe.Out = br.lift1(ai.next)
+        var hasNext = true
+
+        def next(): tpe.Out = {
+          val res = state
+          hasNext = ai.hasNext && bi.hasNext
+          if (hasNext) {
+            state = br.plus(state, br.lift2(bi.next()))
+          }
+          res
+        }
+      }
+    }
+  }
+
+  final case class Cat[T1 <: Top, T2 <: Top, T <: Top](a: Elem[T1], b: Elem[T2])(implicit val tpe: T) extends Elem[T] {
+    def iterator: Iterator[tpe.Out] = ??? // a.mkIter ++ b.mkIter
   }
 
   sealed trait StringTop extends Top {
@@ -78,19 +143,19 @@ object Types {
   final case class ConstInt(x: Int) extends Elem[IntTop] {
     val tpe: IntTop = IntTop
 
-    def mkIter: Iterator[tpe.Out] = Iterator.continually(x)
+    def iterator: Iterator[tpe.Out] = Iterator.continually(x)
   }
 
   final case class ConstIntSeq(xs: Seq[Int]) extends Elem[IntSeqTop] {
     val tpe: IntSeqTop = IntSeqTop
 
-    def mkIter: Iterator[tpe.Out] = Iterator.continually(xs)
+    def iterator: Iterator[tpe.Out] = Iterator.continually(xs)
   }
 
   final case class ConstString(x: String) extends Elem[StringTop] {
     val tpe: StringTop = StringTop
 
-    def mkIter: Iterator[tpe.Out] = Iterator.continually(x)
+    def iterator: Iterator[tpe.Out] = Iterator.continually(x)
   }
 
   implicit def intElem      (x: Int         ): Elem[IntTop      ] = ConstInt(x)
@@ -101,9 +166,31 @@ object Types {
 
   def example(): Unit = {
     // compiles
-    Add(Seq(1, 2), 3)
+    val a = Add[IntSeqTop, IntTop, IntSeqTop](Seq(1, 2), 3)
+    println(a.iterator.take(1).mkString("a: ", ", ", ""))
 
-    Cat("foo", "bar")
+    // compiles
+    val b = Add(Seq(1, 2), 3)
+    println(b.iterator.take(1).mkString("b: ", ", ", ""))
+
+    // compiles
+    val c = Add(1, 3)
+    c.iterator: Iterator[Int] // right
+    println(c.iterator.take(1).mkString("c: ", ", ", ""))
+
+    // compiles
+    val d = Add(1, Seq(2, 3))
+    println(d.iterator.take(1).mkString("d: ", ", ", ""))
+
+    // compiles
+    val e = Add(Seq(1, 2), Seq(3, 4))
+    println(e.iterator.take(1).mkString("e: ", ", ", ""))
+
+    // compiles
+    val f = Series(Seq(2, 3), 4)
+    println(f.iterator.take(3).mkString("f: ", ", ", ""))
+
+    //    Cat("foo", "bar")
 
 //    // ambiguous implicits
 //    Foo(Seq(1, 2), 3.0)
