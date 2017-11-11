2035b6419953c45949eccd59d361934525ea4908
Hanns Holger Rutz <contact@sciss.de>
2017-11-11 18:27:57 +0100

    ....

--- src/main/scala/de/sciss/patterns/graph/FilterPatterns.scala
+++ src/main/scala/de/sciss/patterns/graph/FilterPatterns.scala
@@ -14,19 +14,28 @@
 package de.sciss.patterns
 package graph
 
-import de.sciss.patterns.Types.{IntTop, Top}
+import de.sciss.patterns.Types.Top
 import de.sciss.patterns.graph.impl.Truncate
 
 /** aka `Pfin` */
-final case class Take[T <: Top](in: Pat[T], length: Pat[IntTop])
+final case class Take[T <: Top](in: Pat[T], length: Pat.Int)
   extends Truncate[T] {
 
   protected def truncate(it: Iterator[T#Out], n: Int): Iterator[T#Out] = it.take(n)
 }
 
-final case class Drop[T <: Top](in: Pat[T], length: Pat[IntTop])
+final case class Drop[T <: Top](in: Pat[T], length: Pat.Int)
   extends Truncate[T] {
 
   protected def truncate(it: Iterator[T#Out], n: Int): Iterator[T#Out] = it.drop(n)
 }
 
+final case class Stutter[T <: Top](n: Pat.Int, in: Pat[T])
+  extends Pattern[T] {
+
+  def iterator(implicit ctx: Context): Iterator[T#Out] = {
+    val nIt  = n .expand
+    val inIt = in.expand
+    (inIt zip nIt).flatMap { case (xi, ni) => Iterator.fill(ni)(xi) }
+  }
+}
