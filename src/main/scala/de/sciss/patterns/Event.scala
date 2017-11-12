7ea3f0f8fa74e954e3f849bd4013f03ef602deea
Hanns Holger Rutz <contact@sciss.de>
2017-11-12 22:53:51 +0100

    ...

--- src/main/scala/de/sciss/patterns/Event.scala
+++ src/main/scala/de/sciss/patterns/Event.scala
@@ -15,6 +15,21 @@ package de.sciss.patterns
 
 import de.sciss.patterns.Types.Top
 
+object Event {
+  private def getOrElseDouble(m: Event#Out, key: String, default: => Double): Double =
+    m.get(key) match {
+      case Some(d: Double)  => d
+      case Some(i: Int)     => i.toDouble
+      case _                => default
+    }
+
+  def delta(m: Event#Out): Double =
+    getOrElseDouble(m, "delta", {
+      val stretch = getOrElseDouble(m, "stretch", 1.0)
+      val dur     = getOrElseDouble(m, "dur"    , 0.0)
+      stretch * dur
+    })
+}
 trait Event extends Top {
   type Out = Map[String, _]
 }
--- src/main/scala/de/sciss/patterns/graph/Ppar.scala
+++ src/main/scala/de/sciss/patterns/graph/Ppar.scala
@@ -14,10 +14,45 @@
 package de.sciss.patterns
 package graph
 
+import scala.collection.{AbstractIterator, breakOut}
+import scala.collection.immutable.{SortedMap => ISortedMap}
+
 final case class Ppar(list: Seq[Pat.Event], repeats: Pat.Int = 1, offset : Pat.Int = 0)
   extends Pattern[Event] {
 
   type Out = Event#Out
 
-  def iterator(implicit ctx: Context): Iterator[Out] = ???
+  def iterator(implicit ctx: Context): Iterator[Out] = {
+    val pq0: ISortedMap[Double, Iterator[Out]] = list.flatMap { pat =>
+      val it = pat.expand
+      if (it.hasNext) Some(0.0 -> it) else None
+    } (breakOut)
+
+    new AbstractIterator[Out] {
+      private[this] var pq    = pq0
+      private[this] var done  = pq0.isEmpty
+      private[this] var elem: Out = _
+
+      private def advance(): Unit =
+        pq.headOption match {
+          case Some((time, it)) =>
+            elem  = it.next()
+            val d = Event.delta(elem)
+            if (d >= 0.0 && it.hasNext) pq = pq.tail + ((time + d) -> it)
+
+          case None => done = true
+        }
+
+      def hasNext: Boolean = !done
+
+      advance()
+
+      def next(): Out = {
+        if (done) throw new NoSuchElementException("next on empty iterator")
+        val res = elem
+        advance()
+        res
+      }
+    }
+  }
 }
