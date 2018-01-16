e03d43ce13f32f01a93aedf36ce161a4043d5335
Hanns Holger Rutz <contact@sciss.de>
2018-01-16 17:35:28 +0100

    ...

--- src/test/scala/de/sciss/patterns/ResetExample.scala
+++ src/test/scala/de/sciss/patterns/ResetExample.scala
@@ -6,7 +6,10 @@ import scala.swing.Swing
 
 /*
 
-  Reset a brownian walk every ten steps, four times
+  Reset a brownian walk every ten steps, four times.
+  
+  ex1: Brown is inside the reset scope
+  ex2: Brown is outside the reset scope
 
  */
 object ResetExample {
@@ -14,9 +17,16 @@ object ResetExample {
 
     import graph._
 
-    val outer = Graph {
+    val ex1 = Graph {
       Pat.seqFill(4) { _ =>
         val b = Brown(0, 100, 2)
+        b.take(10)
+      }
+    }
+
+    val ex2 = Graph {
+      val b = Brown(0, 100, 2)
+      Pat.seqFill(4) { _ =>
         b.take(10)
       }
     }
@@ -24,8 +34,12 @@ object ResetExample {
     implicit val ctx: Context = Context()
 //    outer.iterator.foreach(println)
 
+    def plot(g: Pat.Int, title: String): Unit =
+      g.iterator.toList.plot(title = title, discrete = true)
+
     Swing.onEDT {
-      outer.iterator.toList.plot(discrete = true)
+      plot(ex1, "ex1")
+      plot(ex2, "ex2")
     }
   }
 }
