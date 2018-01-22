a67c0e4698d7a3a1a907a56baa86ac1b5155a5a9
Hanns Holger Rutz <contact@sciss.de>
2018-01-22 13:25:20 +0100

    ok; now we have to cast Tx which sucks

--- core/src/main/scala/de/sciss/patterns/graph/PatMap.scala
+++ core/src/main/scala/de/sciss/patterns/graph/PatMap.scala
@@ -14,16 +14,16 @@
 package de.sciss.patterns
 package graph
 
-import de.sciss.patterns.Types.Top
+import de.sciss.patterns.Types.{Aux, Top}
 
 final case class PatMap[T1 <: Top, T <: Top](outer: Pat[Pat[T1]], it: It[T1], inner: Graph[T])
   extends Pattern[Pat[T]] {
 
-  def iterator[Tx](implicit ctx: Context[Tx]): Stream[Tx, Pat[T] /* Stream[Tx, T#Out] */] = new Stream[Tx, Pat[T] /* Stream[Tx, T#Out] */] {
-    private[this] val outerStream: Stream[Tx, Stream[Tx, T1#Out]] = ??? // outer.expand[Tx]
-    private[this] val innerStream: Stream[Tx, T#Out]              = inner.expand[Tx]
+  def iterator[Tx](implicit ctx: Context[Tx]): Stream[Tx, Pat[T]] = new Stream[Tx, Pat[T]] {
+    private[this] val outerStream: Stream[Tx, Pat[T1]]  = outer.expand[Tx]
+    private[this] val innerStream: Stream[Tx, T#Out]    = inner.expand[Tx]
 
-    private[this] val mapStream  = ctx.newVar[Stream[Tx, T#Out]](null)
+    private[this] val patMapInner   = ctx.newVar[Pat[T]](null)
 
     private[this] val hasMapStream  = ctx.newVar(false)
     private[this] val _valid        = ctx.newVar(false)
@@ -44,38 +44,47 @@ final case class PatMap[T1 <: Top, T <: Top](outer: Pat[Pat[T1]], it: It[T1], in
       _hasNext()
     }
 
-    private final class MapStream(itStream: Stream[Tx, T1#Out]) extends Stream[Tx, T#Out] {
-      ctx.setOuterStream[T1#Out](it.token, itStream)
+    private final case class PatMapInner(itPat: Pat[T1]) extends Pat[T] {
 
-      def reset()(implicit tx: Tx): Unit = ()
+      private[patterns] def aux: List[Aux] = Nil
 
-      def hasNext(implicit tx: Tx): Boolean = itStream.hasNext && innerStream.hasNext
+      private[patterns] def expand[Tx1](implicit ctx: Context[Tx1]): Stream[Tx1, T#Out] = iterator
+      def embed                   [Tx1](implicit ctx: Context[Tx1]): Stream[Tx1, T#Out] = iterator
 
-      def next()(implicit tx: Tx): T#Out = innerStream.next()
+      def iterator[Tx1](implicit ctx: Context[Tx1]): Stream[Tx1, T#Out] = new Stream[Tx1, T#Out] {
+        private[this] val itStream: Stream[Tx1, T1#Out] = itPat.expand
+
+        ctx.setOuterStream[T1#Out](it.token, itStream)
+
+        def reset()(implicit tx: Tx1): Unit    = ()
+
+        // XXX TODO --- how to get rid of the casting?
+        def hasNext(implicit tx: Tx1): Boolean = itStream.hasNext && innerStream.hasNext(tx.asInstanceOf[Tx])
+        def next ()(implicit tx: Tx1): T#Out   = innerStream.next()(tx.asInstanceOf[Tx])
+      }
     }
 
     private def advance()(implicit tx: Tx): Unit = {
       if (!hasMapStream()) {
         _hasNext() = outerStream.hasNext
         if (_hasNext()) {
-          val itStream    = outerStream.next()
-          mapStream()     = new MapStream(itStream)
+          val itPat       = outerStream.next()
+          patMapInner()   = PatMapInner(itPat)
           hasMapStream()  = true
           innerStream.reset()
         }
       }
     }
 
-    def next()(implicit tx: Tx): Pat[T] /* Stream[Tx, T#Out] */ = {
+    def next()(implicit tx: Tx): Pat[T] = {
       validate()
       advance()
       if (!_hasNext()) Stream.exhausted()
-      val res = mapStream()
+      val res = patMapInner()
       hasMapStream() = false
       _hasNext() = outerStream.hasNext
 //      advance()
       res
-      ???
     }
   }
 }
