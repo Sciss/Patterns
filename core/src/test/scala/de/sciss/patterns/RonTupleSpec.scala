a0a83bdc4c15bfec9341544831d2a444c890763e
Hanns Holger Rutz <contact@sciss.de>
2018-02-17 21:22:24 +0100

    well, fold-left is not the culprit

--- core/src/test/scala/de/sciss/patterns/RonTupleSpec.scala
+++ core/src/test/scala/de/sciss/patterns/RonTupleSpec.scala
@@ -15,96 +15,96 @@ class RonTupleSpec extends PatSpec {
       }
     }
 
-  "The directProduct example" should work in {
-    //    showStreamLog = true
-
-    val aInSeq  = Seq(Seq(1, 2, 3), Seq(4, 5, 6))
-    val bInSeq  = Seq(7, 8)
-    val plain   = directProduct_Seq(aInSeq, bInSeq)
-    assert(plain === Seq(Seq(1, 2, 3, 7), Seq(1, 2, 3, 8), Seq(4, 5, 6, 7), Seq(4, 5, 6, 8)))
-
-    val outPat = Graph {
-      val aInPat: Pat[Pat[Int]]  = aInSeq.map(xs => Pat[Int](xs: _*))
-      val bInPat: Pat[Int]       = Pat[Int](bInSeq: _*)
-      directProduct_Pat(aInPat, bInPat)
-    }
-    import ctx.tx
-    val res = outPat.expand.map { in =>
-      val i = in.expand.toList
-      i
-    }.toList
-
-    assert(res === plain)  // XXX TODO fails
-
-    /*
-      Se we have
-
-        List(List(1, 2, 3, 7), List(8), List(4, 5, 6, 7), List(8))
-
-      instead of
-
-        List(List(1, 2, 3, 7), List(1, 2, 3, 8), List(4, 5, 6, 7), List(4, 5, 6, 8))
-
-      if we create a copy of `v`, the output is
-
-        List(List(1, 2, 3, 7), List(1, 2, 3, 8), List(1, 2, 3, 7), List(1, 2, 3, 8))
-
-     */
-  }
-
-  "The extract example" should work in {
-    // collects the indices of every occurrence of elements of t in s
-    def extract_Seq[A](s: Seq[A], t: Seq[A]): Seq[Seq[Int]] =
-      t.map { tj =>
-        s.zipWithIndex.collect {
-          case (b, i) if b == tj => i
-        }
-      }
-
-    // collects the indices of every occurrence of elements of t in s
-    def extract_Pat[A](s: Pat[A], t: Pat[A]): Pat[Pat[Int]] =
-      t.bubble.map { tj: Pat[A] =>
-        val sr        = s.recur()
-//        val indices   = s.recur().indexOfSlice(tj)
-//        val indicesF  = FilterSeq(indices, indices >= 0)
-        val same      = sr sig_== Repeat(tj)
-        val indices   = sr.indices
-        val indicesF  = FilterSeq(indices, same)
-        indicesF
-      }
-
-    val as1     = Seq(1, 5, 2, 3, 4)
-    val bs1     = Seq(4, 5, 6)
-    val plain1  = extract_Seq(as1, bs1)
-    assert(plain1 === List(List(4), List(1), List()))
-
-    val pat1 = Graph {
-      extract_Pat(Pat.Int(as1: _*), Pat.Int(bs1: _*))
-    }
-
-    evalH(pat1) shouldBe plain1
-
-    val as2     = Seq(-10.4, -3.2, -8.0, -3.2, -0.8, -3.2, -10.4, -10.4)
-    val bs2     = Seq(-10.4, -8.0, -3.2)
-    val plain2  = extract_Seq(as2, bs2)
-    assert(plain2 === Seq(Seq(0, 6, 7), Seq(2), Seq(1, 3, 5)))
-
-    val pat2 = Graph {
-      extract_Pat(Pat.Double(as2: _*), Pat.Double(bs2: _*))
-    }
-
-    evalH(pat2) shouldBe plain2
-  }
-
-  "The allTuples example" should work in {
-//    def directProduct_Pat[A <: Top](a: Pat[Pat[A]], b: Pat[A]): Pat[Pat[A]] =
-//      a.flatMap { v: Pat[A] =>
-//        val br = b // .recur()
-//        br.bubble.map { w: Pat[A] =>
-//          val vr = v.recur()
-//          vr ++ w
+//  "The directProduct example" should work in {
+//    //    showStreamLog = true
+//
+//    val aInSeq  = Seq(Seq(1, 2, 3), Seq(4, 5, 6))
+//    val bInSeq  = Seq(7, 8)
+//    val plain   = directProduct_Seq(aInSeq, bInSeq)
+//    assert(plain === Seq(Seq(1, 2, 3, 7), Seq(1, 2, 3, 8), Seq(4, 5, 6, 7), Seq(4, 5, 6, 8)))
+//
+//    val outPat = Graph {
+//      val aInPat: Pat[Pat[Int]]  = aInSeq.map(xs => Pat[Int](xs: _*))
+//      val bInPat: Pat[Int]       = Pat[Int](bInSeq: _*)
+//      directProduct_Pat(aInPat, bInPat)
+//    }
+//    import ctx.tx
+//    val res = outPat.expand.map { in =>
+//      val i = in.expand.toList
+//      i
+//    }.toList
+//
+//    assert(res === plain)  // XXX TODO fails
+//
+//    /*
+//      Se we have
+//
+//        List(List(1, 2, 3, 7), List(8), List(4, 5, 6, 7), List(8))
+//
+//      instead of
+//
+//        List(List(1, 2, 3, 7), List(1, 2, 3, 8), List(4, 5, 6, 7), List(4, 5, 6, 8))
+//
+//      if we create a copy of `v`, the output is
+//
+//        List(List(1, 2, 3, 7), List(1, 2, 3, 8), List(1, 2, 3, 7), List(1, 2, 3, 8))
+//
+//     */
+//  }
+//
+//  "The extract example" should work in {
+//    // collects the indices of every occurrence of elements of t in s
+//    def extract_Seq[A](s: Seq[A], t: Seq[A]): Seq[Seq[Int]] =
+//      t.map { tj =>
+//        s.zipWithIndex.collect {
+//          case (b, i) if b == tj => i
 //        }
 //      }
+//
+//    // collects the indices of every occurrence of elements of t in s
+//    def extract_Pat[A](s: Pat[A], t: Pat[A]): Pat[Pat[Int]] =
+//      t.bubble.map { tj: Pat[A] =>
+//        val sr        = s.recur()
+////        val indices   = s.recur().indexOfSlice(tj)
+////        val indicesF  = FilterSeq(indices, indices >= 0)
+//        val same      = sr sig_== Repeat(tj)
+//        val indices   = sr.indices
+//        val indicesF  = FilterSeq(indices, same)
+//        indicesF
+//      }
+//
+//    val as1     = Seq(1, 5, 2, 3, 4)
+//    val bs1     = Seq(4, 5, 6)
+//    val plain1  = extract_Seq(as1, bs1)
+//    assert(plain1 === List(List(4), List(1), List()))
+//
+//    val pat1 = Graph {
+//      extract_Pat(Pat.Int(as1: _*), Pat.Int(bs1: _*))
+//    }
+//
+//    evalH(pat1) shouldBe plain1
+//
+//    val as2     = Seq(-10.4, -3.2, -8.0, -3.2, -0.8, -3.2, -10.4, -10.4)
+//    val bs2     = Seq(-10.4, -8.0, -3.2)
+//    val plain2  = extract_Seq(as2, bs2)
+//    assert(plain2 === Seq(Seq(0, 6, 7), Seq(2), Seq(1, 3, 5)))
+//
+//    val pat2 = Graph {
+//      extract_Pat(Pat.Double(as2: _*), Pat.Double(bs2: _*))
+//    }
+//
+//    evalH(pat2) shouldBe plain2
+//  }
+
+  "The allTuples example" should work in {
+    def directProduct_Pat[A](a: Pat[Pat[A]], b: Pat[A]): Pat[Pat[A]] =
+      a.flatMap { v: Pat[A] =>
+        val br = b.recur()
+        br.bubble.map { w: Pat[A] =>
+          val vr = v.recur()
+          vr ++ w
+        }
+      }
 
     def allTuples_Seq[A](x: Seq[Seq[A]]): Seq[Seq[A]] = {
       val size = x.size
@@ -132,6 +132,13 @@ class RonTupleSpec extends PatSpec {
       }
     }
 
+    def allTuples_Pat1(): Pat[Pat[Int]] = {
+      val y0 = Pat(Pat(0, 6, 7))
+      val y1 = directProduct_Pat(y0, Pat(2))
+      val y2 = directProduct_Pat(y1, Pat(1, 3, 5))
+      y2
+    }
+
     val in  = Seq(Seq(0, 6, 7), Seq(2), Seq(1, 3, 5))
     val out = Seq(Seq(0, 2, 1), Seq(0, 2, 3), Seq(0, 2, 5), Seq(6, 2, 1), Seq(6, 2, 3), Seq(6, 2, 5), Seq(7, 2, 1),
       Seq(7, 2, 3), Seq(7, 2, 5))
@@ -142,10 +149,14 @@ class RonTupleSpec extends PatSpec {
     val plainOut2 = allTuples_Seq2(in)
     plainOut2 shouldBe out
 
+//    val patOut = Graph {
+//      val inPat0 = in.map(xs => Pat.Int(xs: _*))
+//      val inPat: Pat[Pat[Int]] = inPat0
+//      allTuples_Pat(inPat)
+//    }
+
     val patOut = Graph {
-      val inPat0 = in.map(xs => Pat.Int(xs: _*))
-      val inPat: Pat[Pat[Int]] = inPat0
-      allTuples_Pat(inPat)
+      allTuples_Pat1()
     }
 
     evalH(patOut) shouldBe out
