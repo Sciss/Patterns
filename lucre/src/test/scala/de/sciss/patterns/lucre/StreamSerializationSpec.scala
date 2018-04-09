42f993b2f94a824b642c4e571b53d96e15af1e6a
Hanns Holger Rutz <contact@sciss.de>
2018-04-09 11:28:19 +0200

    real serialization spec

--- lucre/src/test/scala/de/sciss/patterns/lucre/StreamSerializationSpec.scala
+++ lucre/src/test/scala/de/sciss/patterns/lucre/StreamSerializationSpec.scala
@@ -1,9 +1,9 @@
 package de.sciss.patterns
 package lucre
 
-import de.sciss.lucre.stm.Durable
 import de.sciss.lucre.stm.store.BerkeleyDB
-import de.sciss.serial.{DataInput, DataOutput}
+import de.sciss.lucre.stm.{Durable, Plain}
+import de.sciss.patterns
 import org.scalatest.{Matchers, Outcome, fixture}
 
 class StreamSerializationSpec extends fixture.FlatSpec with Matchers {
@@ -28,18 +28,29 @@ class StreamSerializationSpec extends fixture.FlatSpec with Matchers {
       Brown(70, 120, 3).midicps
     }
 
-    val values = sys.step { implicit tx =>
-      implicit val c: Context[S] = Context[S]
-      val stream0: Stream[S, Double] = c.expand(g)
+    val seed = 666L
 
-      val out = DataOutput()
-      stream0.write(out)
-      val arr = out.toByteArray
-      val in = DataInput(arr)
-      val stream1 = Stream.read[S, Double](in, ())
-      stream1.toIterator.take(10).toList
+    val values = {
+      implicit val c: patterns.Context[Plain] = patterns.Context()
+      c.setRandomSeed(seed)
+      c.expand(g).toIterator.take(10).toList
     }
 
-    println(values)
+    implicit val c: Context[S] = sys.step { implicit tx => Context[S] }
+
+    val stH = sys.step { implicit tx =>
+      c.setRandomSeed(seed)
+      val st: Stream[S, Double] = c.expand(g)
+      tx.newHandle(st)
+    }
+
+    val trans = List(3, 3, 3, 1).flatMap { n =>
+      sys.step { implicit tx =>
+        val st = stH()
+        List.fill(n)(st.next())
+      }
+    }
+
+    assert(values === trans)
   }
 }
